0bb28ee3cd1b898f8d4be723c18ccfe7765701d0
JC Brand <jc@opkode.com>
2025-08-03 08:58:08 +0200

    Fix TypeScript warning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EdgesModule } from './edges/edges.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Edge } from './edge.entity';
-import { EdgesModule } from './edges/edges.module';
-import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 5432,
+      port:
+        typeof process.env.DB_PORT !== 'undefined'
+          ? parseInt(process.env.DB_PORT)
+          : 5432,
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_NAME || 'postgres',
